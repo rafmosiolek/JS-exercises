98ee98dbf84b5dca610111997974415c511c1b23
RafM <rkjmosiolek@gmail.com>
2017-02-02 21:28:10 +0100

    logical operators

--- script.js
+++ script.js
@@ -35,12 +35,12 @@ The block is delimited by a pair of curly brackets and may optionally be labelle
 /*
 COMPARISON OPERATORS
 
-=== equal to
-!== not equal to
-> greater than
-< less than
-<= less than or equal to
->= greater than or equal to
+=== equal to;
+!== not equal to;
+> greater than;
+< less than;
+<= less than or equal to;
+>= greater than or equal to;
 */
 
 var myName = "Raf";
@@ -52,6 +52,21 @@ if (10*224/16+158===1) {
     console.log("The condition is true");
 } else {
     console.log("The condition is false"); // the condition is obviously false, so the second code block will run
+}
+
+/*
+LOGICAL OPERATORS
+
+&& both must be true;
+|| either can be true;
+! opposite what it really is
+
+*/
+
+var coffee = true
+var milk = "steamed microfoam"
+if (coffee && milk === "steamed microfoam") {
+    console.log("Delicious flat white!") // Flat white is prepared by pouring steamed milk in consistency of microfoam over a double shot of epsresso
 }
 
 
