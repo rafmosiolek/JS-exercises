d22e5f5b2b16ff58e1649f73cabc3e39fa044aea
RafM <rkjmosiolek@gmail.com>
2017-02-02 21:50:32 +0100

    control flow review

--- script.js
+++ script.js
@@ -134,9 +134,26 @@ It is not necessary to break the last case in a switch block. The block breaks (
 */
 
 
+/*
+
+CONTROL FLOW REVIEW
+
+if/else statements make binary decisions and execute separate code based on a condition.
+
+We can add extra conditions with to if/else statements with else if conditions.
+
+Switch statements make complicated if/else statements easier to read, however they achieve the same result as if/else statements.
+
+Comparison operators, like <, >, <=, and >= can compare two variables. After they compare, they always return either true or false.
+
+Logical Operators, like &&, ||, !==, and !, can compare two variables to see if a certain condition exists:
+&& checks if both sides are true.
+|| checks if either side is true.
+!== checks if both sides are not equal.
+! changes a variable that is true to false, and vice versa.
 
 
-
+*/
 
 
 
