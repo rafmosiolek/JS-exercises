16137ca162675ec4345e667fc2f2102f7e54454d
RafM <rkjmosiolek@gmail.com>
2017-02-05 00:09:44 +0100

    Scope review

--- script.js
+++ script.js
@@ -344,10 +344,15 @@ var myApartment = function() {
   console.log('Mail box: ' + mailBoxNumber + ', Laundry:' + laundryRoom);
 };
 
+/*
 
+SCOPE REVIEW
 
+Scope is the idea in programming that some variables are acessible/inaccessible from other parts of the program.
+Global Scope refers to variables that are accessible to every part of the program.
+Functional Scope refers to variables created inside functions, which are not accessible outside of its block.
 
-
+*/
 
 
 
