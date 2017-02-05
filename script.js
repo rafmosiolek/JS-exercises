965871cab433bc5a7e208517d183df9a13629974
RafM <rkjmosiolek@gmail.com>
2017-02-05 12:40:21 +0100

    Arrays review

--- script.js
+++ script.js
@@ -402,7 +402,16 @@ arrayName.pop();
 
 console.log(arrayName); // Output = ["item0", "item1"]; item2 was deleted.
 
+/*
 
+ARRAYS REVIEW:
+
+ Arrays are lists and are a way to store data in JavaScript. Each item inside of an array is at a numbered position. Arrays are created with brackets [].
+We can access one item in an array using it's numbered position, with syntax like: myArray[0].
+Arrays have a length property, which allows you to see how many items are in an array.
+Arrays also have their own methods, including push and pop, which add and subtract items from an array, respectively.
+
+*/
 
 
 
