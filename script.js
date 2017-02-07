e40554f00fdee350fa05aef1f5bf1a485b1684fc
RafM <rkjmosiolek@gmail.com>
2017-02-07 23:11:36 +0100

    Battleship exercise

--- script.js
+++ script.js
@@ -600,9 +600,58 @@ function main() {
 
 $(document).ready(main);
 
+/*
+- toggle will toggle an element on and off the page.
+- $(this) will select the specific element that was clicked if placed inside a click function.
+- toggleClass can toggle a class on and off.
+- We can select elements next to each other with next.
+- text will replace a DOM element's text with text we specify.
+- slideToggle will make an element slide into and out of the page with an animation.
+*/
 
 
 
+
+
+
+
+
+
+
+
+// BATTLESHIP EXERCISE
+
+
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location1 + 2;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!");
+    } else {
+        guesses = guesses + 1;
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("HIT!");
+            hits = hits + 1;
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+        } else {
+            alert("MISS");
+        }
+    }
+}
+var stats = "You took " + guesses + " guesses to sink the battleship, " +
+            "which means your shooting accuracy was " + (3/guesses);
+alert(stats);
 
 
 
