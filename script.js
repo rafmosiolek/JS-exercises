8ac1ec577ac9f46b8d9cfaa628b7256360eb557f
RafM <rkjmosiolek@gmail.com>
2017-02-15 01:13:45 +0100

    simple calculator only js

--- script.js
+++ script.js
@@ -803,3 +803,85 @@ else {
 
 
 
+
+
+
+
+
+/////////////////////////////////////      CALCULATOR
+
+
+
+// Constant and global variables:
+
+Memory  = "0";      // initialise memory variable
+Current = "0";      //   and value of Display ("current" value)
+Operation = 0;      // Records code for eg * / etc.
+MAXLENGTH = 30;     // maximum number of digits before decimal!
+
+
+/////// Invidual event handlers:
+
+// AddDigit - adding a digit to a display
+
+
+function AddDigit(dig)          //ADD A DIGIT TO DISPLAY (kept as 'Current')
+ {if (Current.length > MAXLENGTH)
+       { Current = "Aargh! Too long"; //limit length
+       } else
+       { if (    (eval(Current) == 0)
+              && (Current.indexOf(".") == -1)
+            )
+           { Current = dig;
+           } else
+           { Current = Current + dig;
+           };
+    }; };
+   document.Calculator.Display.value = Current;
+ }
+
+// Decimal point
+
+
+function Dot()                  //PUT IN "." if appropriate.
+ {
+  if ( Current.length == 0)     //no leading ".", use "0."
+    { Current = "0.";
+    } else
+    {  if ( Current.indexOf(".") == -1)
+         { Current = Current + ".";
+    };   };
+  document.Calculator.Display.value = Current;
+ }
+
+// Clear all and clear entry buttons:
+
+
+function Clear()                //CLEAR ENTRY
+ { Current = "0";
+   document.Calculator.Display.value = Current;
+ }
+
+function AllClear()             //Clear ALL entries!
+ { Current = "0";
+   Operation = 0;                //clear operation
+   Memory = "0";                  //clear memory
+   document.Calculator.Display.value = Current;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
