c3afb8d077657d508da1c611974d6dd0c0b4d685
RafM <rkjmosiolek@gmail.com>
2017-02-17 10:34:55 +0100

    rock paper scissor game else/if statements exercise

--- script.js
+++ script.js
@@ -946,6 +946,60 @@ function Calculate()            //PERFORM CALCULATION (= button)
 
 
 
+ ///////////// ROCK, PAPER, SCISSOR GAME via codeacademy
+
+
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+
+if (computerChoice >= 0 && computerChoice <= 0.33){
+    console.log("Rock");
+} else if (computerChoice >= 0.34 && computerChoice <= 0.66) {
+    console.log("Paper");
+} else {
+    console.log("Scissors");
+};
+
+var compare = function(choice1, choice2) {
+    if (choice1 === choice2) {
+    return "The result is a tie!";
+}
+    else if (choice1 === "rock") {
+        
+        if (choice2 === "scissors") {
+        return "rock wins";
+        }
+        
+        else {
+            return "paper wins";
+        }
+    }
+    
+    else if (choice1 === "paper") {
+        
+        if (choice2 === "rock") {
+            return "paper wins";
+        }
+        
+        else {
+            return "scissors wins";
+        }
+    }
+    
+    else if (choice1 === "scissors") {
+        
+        if (choice2 === "rock") {
+        return "rock wins";
+        }
+        
+        else {
+            return "scissors wins";
+        }
+    }
+
+};
+
+console.log(compare(userChoice, computerChoice));
 
 
 
